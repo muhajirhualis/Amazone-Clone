c51a8ccc96e25b6b4d8e5001782d1b0bf98c2fa6
muhajirhualis <muhualis@gmail.com>
2023-12-24 03:49:33 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Checkout from "./Components/Checkout/Checkout";
+import Home from "./Components/Home/Home";
+import Shared from "./Shared";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import { useStateValue } from "./StateProvider";
+import React, { useEffect } from "react";
+import { auth } from "./firebase";
+import Payment from "./Components/Payment/Payment";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Orders from "./Components/Orders/Orders";
+
+const promise = loadStripe(
+  'pk_test_51OOhI5HqWj70JuVug0QYMgjgCqunPeadnGjDJvBQBBW6tjRCNYL86jKs0SBwe9J4REBu9PlcaFWd0XGiI2FMKnNr00cUSagvgT'
+);
+
 
 function App() {
+  const [{ }, dispatch] = useStateValue();
+  useEffect(() => {
+   auth.onAuthStateChanged((authUser) => {
+      
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        });
+      } else {
+        // ¨the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+
+      }
+    })
+
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Shared />}>
+          <Route path="/" element={<Home />} />
+          <Route path="Checkout" element={<Checkout />} />
+          <Route path="Payment" element=
+           
+            {<Elements stripe={promise}>
+            <Payment />
+          </Elements>}
+          
+          />
+          <Route path="orders" element={ <Orders/>} />
+        </Route>
+        <Route path="Login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
